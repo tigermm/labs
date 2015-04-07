e8fd31e32001ed63b58751c8afe803062d0b0617
Mikhail Sobyanin <mihailsobyanin@gmail.com>
2015-04-07 13:21:37 +0600

    Update dictionary.pl

--- prolog/dictionary.pl
+++ prolog/dictionary.pl
@@ -1,23 +1,21 @@
-word("home","dom").
-word("friend","друг").
-word("good","хороший").
-word("mouse",'мышь').
-word('watch','chasi').
+word('home','дом').
+word('friend','друг').
+word('good','хороший').
+word('mouse','мышь').
+word('watch','часы').
 
-menu:-repeat,write("1-All books"),nl,
+menu1:-repeat,write("1-All books"),nl,
 write("2-Find book by name"),nl,
 write("3-Find book by price"),nl,
 write("0-exit"),nl,nl,
 read(N),do(N),N=0.
 
-do(1):-word(X,Y),write(X),write("--"),write(Y),write("--"),nl,fail.
+do(1):-word(X,Y),write(X),write("--"),write(Y),nl,fail.
 
 do(2):-!,write("English word = "),
-readln(X),word(X,Y),
-write(X),write("--"),write(Y),nl,fail.
+read(X),word(X,Y),write(X),write("--"),write(Y),nl,fail.
 
-do(3):-!,write("Russian word = "),readln(Y),
+do(3):-!,write("Russian word = "),read(Y),
 word(X,Y),write(X),write("--"),write(Y),nl,fail.
 
 do(0).
-
