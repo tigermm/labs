ade03edc1da2db8f2f7e437e517fd67f884cff54
Mikhail Sobyanin <mihailsobyanin@gmail.com>
2015-04-29 11:33:36 +0300

    Update multiplication.pl

--- prolog/multiplication.pl
+++ prolog/multiplication.pl
@@ -1,4 +1,5 @@
-multiplication(0,Y,Z):-  Y<Z, write(Z),!.
-multiplication(X,Y,Z):- Z1 is Z + Y, X1 is X-1, multiplication(X1, Y, Z1).
-multiplication(X,Y):-multiplication(X,Y,0).
-summa(X,Y):- Z is X + Y, write(Z).
+multiplication(_,0,0):-  write(0),!.
+multiplication(0,_,0):-  write(0),!.
+multiplication(1,Y,Z,K):-  K is Z + Y, !.
+multiplication(X,Y,Z,K):- Z1 is Z + Y, X1 is X-1, multiplication(X1, Y, Z1,K).
+multiplication(X,Y):-multiplication(X,Y,0,K), write(K).
